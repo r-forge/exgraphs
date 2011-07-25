dd67e9cede538e2d371c26c5c4c01d4615bf9596
masanao (Masanao Yajima) <yajima@stat.ucla.edu>
2011-07-25 13:28:37 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/exgraphs@4 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/exampl.R
+++ pkg/R/exampl.R
@@ -8,7 +8,7 @@ counts=table(cut(x,breaks=c(breakX[-101],Inf)),
 cut(y,breaks=c(breakY[-101],Inf) ))
 xcenter <- (breakX[-1]+breakX[-101])/2
 ycenter <- (breakY[-1]+breakY[-101])/2
-gridScatterPlot(counts,x.center=xcenter, y.center=ycenter,radi=300)
+gridscatterplot(counts,x.center=xcenter, y.center=ycenter,radi=300)
 
 #multiple class
 
@@ -18,7 +18,7 @@ for(i in 1:3){
 counts.array[,,i]=table(cut(x,breaks=c(breakX[-101],Inf)),
 cut(y,breaks=c(breakY[-101],Inf) ))
 }
-gridScatterPlot(counts.array,x.center=xcenter, y.center=ycenter,radi=300,alpha=0.7)
+gridscatterplot(counts.array,x.center=xcenter, y.center=ycenter,radi=300,alpha=0.7)
 
 # with color
-gridScatterPlot(counts.array,x.center=xcenter, y.center=ycenter,radi=300,alpha=0.7,col=c("red", "blue","yellow"))
+gridscatterplot(counts.array,x.center=xcenter, y.center=ycenter,radi=300,alpha=0.7,col=c("red", "blue","yellow"))
